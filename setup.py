5327aa5f16bc8b33961350f757286fa14fd1f851
Dimitrios Dedoussis <ddedoussis@gmail.com>
2020-02-22 21:51:15 +0000

    Add dependencies

--- setup.py
+++ setup.py
@@ -3,20 +3,17 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-with open('requirements.txt') as fp:
+with open("requirements.txt") as fp:
     install_requires = fp.read()
 
 setup(
-    name = 'gressify',
-    version = '0.0.1',
-    author = 'dedoussis',
-    author_email = 'ddedoussis@gmail.com',
-    description = ('Adds a rule on a vpc security group'),
-    license = 'Internal Use Only',
-    keywords = 'aws vpc ec2 security group rule ip ingres egress',
-    install_requires = install_requires.split(),
-    packages = find_packages(),
-    entry_points = {
-        'console_scripts': ['gressify = gressify.__main__:main']
-    }
+    name="gressify",
+    version="0.0.1",
+    author="dedoussis",
+    author_email="ddedoussis@gmail.com",
+    description="Adds a rule on a vpc security group",
+    keywords="aws vpc ec2 security group rule ip ingres egress",
+    install_requires=install_requires.split(),
+    packages=find_packages(),
+    entry_points={"console_scripts": ["gressify = gressify.__main__:main"]},
 )
